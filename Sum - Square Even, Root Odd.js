7e71c01cbd225cea955c3bc7767ae982bae43880
Sarah Bakir <sarahbakir.k@gmail.com>
2021-05-12 06:22:26 +0200

    SUM - Math.qrt()

---
+++ Sum - Square Even, Root Odd.js
@@ -0,0 +1,17 @@
+// Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+
+// The list will never be empty and will only contain values that are greater than or equal to zero.
+
+// Good luck!
+
+const sumSquareEvenRootOdd = ns => {
+    let res = [];
+    for(let i = 0; i < ns.length; i++) {
+      if(ns[i] % 2 === 0) {
+        res.push(ns[i]**2)
+      } else {
+        res.push(Math.sqrt(ns[i]))
+      }
+    }
+    return   Number((eval(res.join('+'))).toFixed(2))
+  };
