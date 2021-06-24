394627eb8f010b2b16bd07f1e8b7b836179fc712
Sarah Bakir <sarahbakir.k@gmail.com>
2021-06-24 22:56:01 +0200

    Herons formula

---
+++ Heron's formula.js
@@ -0,0 +1,9 @@
+// Write function heron which calculates the area of a triangle with sides a, b, and c.
+
+// Heron's formula: sqrt (s * (s - a) * (s - b) * (s - c)), where s = (a + b + c) / 2. Output should have 2 digits precision.
+
+function heron(a,b,c) {
+    let s = (a + b + c) / 2;
+    let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    return area;
+  }
