5b9caaed72084c47d1b73c7467f8622355f02c66
Sarah Bakir <sarahbakir.k@gmail.com>
2021-08-31 18:17:14 +0200

    Sum of digits - 2 solutions

--- Sum of digits.js
+++ Sum of digits.js
@@ -4,8 +4,34 @@
 // Digits can be a number or a string, and you should ensure it is not None and return an empty string.
 // To give you a little more excitement, the program will not only write the result of the sum, but also write all the sums used: 3 + 4 + 3 + 3 = 13.
 
+//solution 1
 function sum(digits) {
     let sum = digits.split('').join(' + ')
     let res = eval(sum)
     return `${sum} = ${res}`;
+  }
+
+  //solution 2
+  function sum(digits) {
+  
+    if(!digits) {
+      return ``;
+    } else if(digits === 0) {
+      return `${digits} = ${digits}`
+    } else {
+  
+    
+    let nums = []
+    let res = 0;
+    
+    for(let i = 0; i < digits.length; i++) {
+      res = res + Number(digits[i])
+      nums.push(digits[i])
+      
+    }
+    
+    return `${nums.join(' + ')} = ${res}`;
+    }
+  
+  
   }
